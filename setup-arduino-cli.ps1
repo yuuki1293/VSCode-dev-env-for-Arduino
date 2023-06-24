4373c70eb0c5a0914f1b995b0ea5ceb2177b535f
yuuki1293 <yuuki1293@outlook.jp>
2023-06-24 23:16:55 +0900

    setup-arduino-cli.ps1を更新

--- setup-arduino-cli.ps1
+++ setup-arduino-cli.ps1
@@ -1,5 +1,14 @@
+if(test-path bin/arduino-cli.exe){
+    echo "Already up to date."
+    exit
+}
+
 if(-not (test-path bin)){
     New-Item -Path bin -ItemType Directory
 }
-Invoke-WebRequest -Uri "https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Windows_64bit.zip" -OutFile  "bin/arduino-cli.zip"
-Expand-Archive -Path bin\arduino-cli.zip -DestinationPath bin\
+
+if(-not (test-path bin/arduino-cli.zip)){
+    Invoke-WebRequest -Uri "https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Windows_64bit.zip" -OutFile  "bin/arduino-cli.zip"
+}
+
+Expand-Archive -Path bin\arduino-cli.zip -DestinationPath bin -Force
